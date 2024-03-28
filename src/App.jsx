f28612217dc6c071c98ff6d9f34a2a37ea00d2b1
Mohammad Adil <mohammadadil3292@gmail.com>
2024-03-28 17:32:53 +0530

    add remaining home page features

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import ButtonGradient from "./assets/svg/ButtonGradient";
 import Hero from "./components/Hero";
 import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,6 +16,10 @@ const App = () => {
         <Hero/>
         <Benefits/>
         <Collaboration/>
+        <Services/>
+        <Pricing/>
+        <Roadmap/>
+        <Footer/>
       </div>
       <ButtonGradient/>
     </>
--- src/constants/index.js
+++ src/constants/index.js
@@ -79,8 +79,9 @@ export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
 
 export const brainwaveServices = [
   "Photo generating",
-  "Photo enhance",
-  "Seamless Integration",
+  "Code generation",
+  "Text Editing",
+  "Video generation",
 ];
 
 export const brainwaveServicesIcons = [
@@ -96,7 +97,7 @@ export const roadmap = [
     id: "0",
     title: "Voice recognition",
     text: "Enable the chatbot to understand and respond to voice commands, making it easier for users to interact with the app hands-free.",
-    date: "May 2023",
+    date: "January 2024",
     status: "done",
     imageUrl: roadmap1,
     colorful: true,
@@ -105,7 +106,7 @@ export const roadmap = [
     id: "1",
     title: "Gamification",
     text: "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently.",
-    date: "May 2023",
+    date: "March 2024",
     status: "progress",
     imageUrl: roadmap2,
   },
@@ -113,7 +114,7 @@ export const roadmap = [
     id: "2",
     title: "Chatbot customization",
     text: "Allow users to customize the chatbot's appearance and behavior, making it more engaging and fun to interact with.",
-    date: "May 2023",
+    date: "Feb 2024",
     status: "done",
     imageUrl: roadmap3,
   },
@@ -121,7 +122,7 @@ export const roadmap = [
     id: "3",
     title: "Integration with APIs",
     text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
-    date: "May 2023",
+    date: "April 2024",
     status: "progress",
     imageUrl: roadmap4,
   },
@@ -289,7 +290,7 @@ export const benefits = [
     title: "Improve everyday",
     text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
     backgroundUrl: "./src/assets/benefits/card-6.svg",
-    iconUrl: benefitIcon2,
+    iconUrl: benefitIcon4,
     imageUrl: benefitImage2,
   },
 ];
