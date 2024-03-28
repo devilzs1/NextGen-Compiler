7df5d50394862d6cc4e6a44d87fa0c7e4fe834b7
Mohammad Adil <mohammadadil3292@gmail.com>
2024-03-28 17:55:04 +0530

    update hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+            Explore the Possibilities of&nbsp;NextGen Compiler&nbsp; with {` `}
             <span className="inline-block relative">
               Brainwave{" "}
               <img
